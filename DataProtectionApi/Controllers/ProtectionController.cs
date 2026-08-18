abc349d02215daef44f70c1a1b8df222cd18de05
Supraja Tangella <supraja.tangella@gmail.com>
2026-08-18 13:53:31 +0530

    commit API

---
+++ DataProtectionApi/Controllers/ProtectionController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.DataProtection;
+
+namespace DataProtectionApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProtectionController : Controller
+    {
+        private readonly IDataProtector _protector;
+
+        public ProtectionController(IDataProtectionProvider provider)
+        {
+            _protector = provider.CreateProtector("DataProtectionApi.v1");
+        }
+
+        [HttpPost("protect")]
+        public IActionResult Protect([FromBody] string value)
+        {
+            try
+            {   if (string.IsNullOrEmpty(value))
+                {
+                    return BadRequest("Value cannot be null or empty.");
+                }
+                else
+                {
+                    var protectedValue = _protector.Protect(value);
+                    return Ok(new
+                    {
+                        original = value,
+                        protectedValue
+                    });
+                }
+            }
+            catch (Exception)
+            {
+                return BadRequest("An error occurred while processing the request.");
+            }
+           
+        }
+
+        [HttpPost("unprotect")]
+        public IActionResult Unprotect([FromBody] string protectedValue)
+        {
+            try
+            {
+                var originalValue = _protector.Unprotect(protectedValue);
+
+                return Ok(new
+                {
+                    protectedValue,
+                    original = originalValue
+                });
+            }
+            catch (Exception)
+            {
+                return BadRequest("Invalid protected value.");
+            }
+        }
+
+    }
+}
